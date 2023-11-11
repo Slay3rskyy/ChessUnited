a59cca1b9b7a16f5dedd63a150a480e8071bc650
mateusz <mbartnikiewicz0@gmail.com>
2023-11-11 20:19:13 +0100

    board is now complete, time to chess

--- board.py
+++ board.py
@@ -4,6 +4,9 @@ import pygame
 def set_board(win, width, height):
 	board = [[""]]
 	
+	chess_black = (30,30,30)
+	chess_white = (205,205,205)
+	
 	for i in range(0, 8):
 		board.append([])
 		for j in range(0, 8):
@@ -11,9 +14,11 @@ def set_board(win, width, height):
 			if (i + j) % 2 == 0:
 				
 				board[i].append("w")
-				pygame.draw.rect(win, (255, 255, 255),
+				pygame.draw.rect(win, chess_white,
 								 pygame.Rect(width // 8 * i, height // 8 * j, width // 8, height // 8))
 			else:
 				
 				board[i].append("b")
-				pygame.draw.rect(win, (0, 0, 0), pygame.Rect(width // 8 * i, height // 8 * j, width // 8, height // 8))
+				pygame.draw.rect(win, chess_black, pygame.Rect(width // 8 * i, height // 8 * j, width // 8, height // 8))
+
+
