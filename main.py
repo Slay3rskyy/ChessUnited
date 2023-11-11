31fde5c11b4094bec36818bc274546535edd2fb0
mateusz <mbartnikiewicz0@gmail.com>
2023-11-11 18:39:14 +0100

    starting the app, off to a new creation

---
+++ main.py
@@ -0,0 +1,4 @@
+import mainFrame
+
+if __name__ == "__main__":
+	mainFrame.main()
---
+++ mainFrame.py
@@ -0,0 +1,41 @@
+import pygame, sys, random
+from pygame.locals import *
+
+pygame.init()
+
+# Colours
+BACKGROUND = (255, 255, 255)
+
+# Game Setup
+FPS = 60
+fpsClock = pygame.time.Clock()
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 300
+
+
+# The main function that controls the game
+def main(width,height,fps):
+	looping = True
+	
+	WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+	pygame.display.set_caption('My Game!')
+
+	fps.
+	# The main game loop
+	while looping:
+		# Get inputs
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+		
+		# Processing
+		# This section will be built out later
+		
+		# Render elements of the game
+		WINDOW.fill(BACKGROUND)
+		pygame.display.update()
+		fpsClock.tick(FPS)
+
+
+main()
