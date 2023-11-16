962674ca06fa66e8383d172a4db5b82ed4c314e2
jakub <kierznowski.jakub@wp.pl>
2023-11-16 15:01:05 +0100

    board is now complete, time to chess

---
+++ pawn_script.py
@@ -0,0 +1,5 @@
+def start_pos(window, position, board_as_2d_list):
+    if type(board_as_2d_list) is not type(list()):
+        raise Exception("did not enter a correct board")
+    else:
+        print("działa")
